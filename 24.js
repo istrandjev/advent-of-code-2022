170812e27ef2ab00ba06138bd95aaccbcd148ce9
Ivaylo Strandjev <ivo.strandjev@microblink.com>
2022-12-24 10:06:02 +0200

    Slight code improvement

--- 24.js
+++ 24.js
@@ -52,12 +52,13 @@ function check_cell(field, cell) {
     return true;
 }
 
-function solve(field) {
+function get_path_length(field, from, target_y, move) {
     let cycle = lcm(field.length - 2, field[0].length - 2);
     const queue = [];
-    queue.push([0, 1, 0]);
+    let source = [from[0], from[1], move]
+    queue.push(source);
     const vis = {};
-    vis[[0, 1, 0]] = 0;
+    vis[source] = 0;
     let best = -1;
     while (queue.length != 0 && best == -1) {
         const c = queue.shift();
@@ -67,29 +68,36 @@ function solve(field) {
             vis[stay] = c_dist + 1;
             queue.push(stay);
         }
-        
-        moves.forEach(move_delta => {
+
+        for (move_delta of moves) {
             const ty = c[0] + move_delta[0];
             const tx = c[1] + move_delta[1];
-            
-            if (ty >= 0 && field[ty][tx] != '#') {
-                const next = [ty, tx, (c[2] + 1) % cycle];
-                if (!vis.hasOwnProperty(next) && check_cell(field, next)) {
-                    vis[next] = c_dist + 1;
-                    if (next[0] == field.length - 1 && best == -1) {
-                        best = c_dist + 1;
-                    }
-                    queue.push(next);
-                }
+
+            if (ty < 0 || ty >= field.length || field[ty][tx] == '#') {
+                continue;
             }
-        });
+            const next = [ty, tx, (c[2] + 1) % cycle];
+            if (!vis.hasOwnProperty(next) && check_cell(field, next)) {
+                vis[next] = c_dist + 1;
+                if (next[0] == target_y && best == -1) {
+                    return c_dist + 1;
+                }
+                queue.push(next);
+            }
+        }
     }
     return best;
 }
 
-// Initialize an array to hold the lines of input
-let lines = [];
+function solve(field) {
+    let first = get_path_length(field, [0, 1], field.length - 1, 0);
+    console.log("Answer to part 1 = " + first);
+    let second = get_path_length(field, [field.length - 1, field[0].length - 2], 0, first);
+    let third = get_path_length(field, [0, 1], field.length - 1, first + second);
+    console.log("Answer to part 2 = " + (first + second + third));
+}
 
+let lines = [];
 const rl = readline.createInterface({
   input: process.stdin,
   output: process.stdout,
@@ -99,6 +107,5 @@ rl.on('line', line => {
   lines.push(line);
 });
 rl.on('close', () => {
-  console.log(solve(lines));
+  solve(lines);
 });
-
